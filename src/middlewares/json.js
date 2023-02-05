079374ed1a23dbddcd8ef1689b51c76fcb874fcc
Rafael Freitas <vidarafael@hotmail.com>
2023-02-05 00:35:49 -0300

    first commit

---
+++ src/middlewares/json.js
@@ -0,0 +1,15 @@
+export async function json(req, res) {
+  const buffer = []
+
+  for await (const chunk of req) {
+    buffer.push(chunk)
+  }
+
+  try {
+    req.body = JSON.parse(Buffer.concat(buffer).toString())
+  } catch (error) {
+    req.body = null
+  }
+
+  res.setHeader('Content-type', 'application/json')
+}
